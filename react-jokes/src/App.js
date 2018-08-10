313335d14c5fd03f471776886f22455be72853f4
emaquino44 <prexy44@yahoo.com>
2018-08-10 11:13:54 -0700

    created login component - still debugging

--- react-jokes/src/App.js
+++ react-jokes/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login.js';
+
+
 
 class App extends Component {
   render() {
@@ -8,14 +12,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Lambda's Jokes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+        {localStorage.getItem('jwt') && (
+        <button onClick={this.logoutHandler}>Log Out</button>)}
+
+             <Route path='/login' component={ Login } />
+
+        </div>
       </div>
     );
   }
+    logoutHandler = event => {
+      localStorage.removeItem('jwt');
+
+      this.props.history.push('/login');
+    };
 }
 
-export default App;
+export default withRouter(App);
